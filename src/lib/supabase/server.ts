971a67b7f9b2c4596e5591841e914293c4591555
Mwalker5722 <theindexcloud@gmail.com>
2025-08-02 14:55:15 -0400

    Update server.ts

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,27 +1,10 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-import { type NextRequest, NextResponse } from 'next/server'
 
-export const createClient = (request?: NextRequest) => {
-  let cookieStore;
+export function createClient() {
+  const cookieStore = cookies()
 
-  if (request) {
-    // If a request is provided, use it to get the cookies.
-    // This is for use in middleware.
-    cookieStore = request.cookies
-  } else {
-    // Otherwise, get the cookies from the Next.js headers store.
-    // This is for use in Server Components.
-    cookieStore = cookies()
-  }
-
-  const response = NextResponse.next({
-    request: {
-      headers: request?.headers || new Headers(),
-    },
-  });
-
-  const supabase = createServerClient(
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
@@ -30,29 +13,22 @@ export const createClient = (request?: NextRequest) => {
           return cookieStore.get(name)?.value
         },
         set(name: string, value: string, options: CookieOptions) {
-          if (request) {
-            // In middleware, we can set the cookie on the request and response.
-            request.cookies.set({ name, value, ...options })
-            response.cookies.set({ name, value, ...options })
-          } else {
-            // In Server Components, we can only set the cookie on the response.
-            // We use the `cookies()` function from `next/headers` to do this.
-            cookies().set({ name, value, ...options })
+          try {
+            cookieStore.set({ name, value, ...options })
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing user sessions.
           }
         },
         remove(name: string, options: CookieOptions) {
-          if (request) {
-            // In middleware, we can delete the cookie on the request and response.
-            request.cookies.set({ name, value: '', ...options })
-            response.cookies.set({ name, value: '', ...options })
-          } else {
-            // In Server Components, we can only delete the cookie on the response.
-            cookies().set({ name, value: '', ...options })
+          try {
+            cookieStore.set({ name, value: '', ...options })
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing user sessions.
           }
         },
       },
     }
   )
-
-  return { supabase, response }
 }
